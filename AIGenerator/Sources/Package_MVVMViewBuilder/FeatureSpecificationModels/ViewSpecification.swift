9f81fb676b3b9d151b78380764ac71d037820f6f
Michael Baldock <michaelb1983@gmail.com>
2025-01-24 10:14:09 +0000

    eng-health: organised View Spec and View Data

--- AIGenerator/Sources/Package_MVVMViewBuilder/FeatureSpecificationModels/ViewSpecification.swift
+++ AIGenerator/Sources/Package_MVVMViewBuilder/FeatureSpecificationModels/ViewSpecification.swift
@@ -4,11 +4,11 @@ struct ViewSpecification {
     let viewName: String
     let viewFolderPath: String
     let models: [ViewData]
-}
-
-struct ViewData {
-    let variableName: String
-    let modelType: String
-    let modelPath: String
-    var isCollection: Bool = false
+    
+    struct ViewData {
+        let variableName: String
+        let modelType: String
+        let modelPath: String
+        var isCollection: Bool = false
+    }
 }
