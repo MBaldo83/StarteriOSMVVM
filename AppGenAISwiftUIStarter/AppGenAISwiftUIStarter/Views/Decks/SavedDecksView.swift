7b76d7dba5f955ae81c3e6dd29ee38b4aa1368fb
Michael Baldock <michaelb1983@gmail.com>
2025-02-20 08:02:36 +0000

    feat: updated look of decks view

--- AppGenAISwiftUIStarter/AppGenAISwiftUIStarter/Views/Decks/SavedDecksView.swift
+++ AppGenAISwiftUIStarter/AppGenAISwiftUIStarter/Views/Decks/SavedDecksView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import CoreData
 
 struct SavedDecksView: View {
     @Environment(Router.self) private var router: Router
@@ -6,6 +7,7 @@ struct SavedDecksView: View {
     @FetchRequest(sortDescriptors: [
         SortDescriptor(\.name)
     ]) var decks: FetchedResults<Deck>
+    @Environment(\.managedObjectContext) var context
     
     var body: some View {
         SavedDecksContentView(
@@ -15,7 +17,8 @@ struct SavedDecksView: View {
             },
             navigateToDeckDetails: { deck in
                 router.navigateTo(.deckDetails(deck: deck))
-            }
+            },
+            deleteDeck: { $0.delete(in: context) }
         )
     }
 }
@@ -24,19 +27,63 @@ struct SavedDecksContentView: View {
     let decks: FetchedResults<Deck>
     let presentGenerator: () -> Void
     let navigateToDeckDetails: (Deck) -> Void
-
+    let deleteDeck: (Deck) -> Void
+    
     var body: some View {
-        VStack {
-            Button(action: presentGenerator) {
-                Text("Generate")
-            }
-            ForEach(decks) { deck in
-                Button(action: {
-                    navigateToDeckDetails(deck)
-                }) {
-                    Text(deck.name ?? "")
+        ZStack {
+            // Main content
+            List {
+                ForEach(decks) { deck in
+                    Button(action: {
+                        navigateToDeckDetails(deck)
+                    }) {
+                        HStack {
+                            Text(deck.name ?? "")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
                 }
+                .onDelete(perform: delete)
             }
+            
+            // Bottom button
+            VStack {
+                Spacer()
+                Button(action: presentGenerator) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                        Text("Generate New Deck")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                .padding()
+            }
+        }
+    }
+    
+    private func delete(at offsets: IndexSet) {
+        for index in offsets {
+            let deckToDelete = decks[index]
+            deleteDeck(deckToDelete)
+        }
+    }
+}
+
+extension Deck {
+    func delete(
+        in context: NSManagedObjectContext
+    ) {
+        context.delete(self)
+        do {
+            try context.save()
+        } catch {
+            print("Failed to delete person: \(error)")
         }
     }
 }
